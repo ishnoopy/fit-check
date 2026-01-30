4bda58ed7dd3694549203e5b34d927981bb01c9b
Ruel Aldrin Guasa <114205020+ishnoopy@users.noreply.github.com>
2026-01-30 15:43:58 +0800

    chore: handle exercises in workout model

--- backend/src/models/exercise.model.ts
+++ backend/src/models/exercise.model.ts
@@ -3,42 +3,43 @@ import mongoose, { model } from "mongoose";
 // e.g. Bench Press, Squats, Deadlift, etc.
 
 export interface IExercise {
-    id?: string;
-    userId: mongoose.Schema.Types.ObjectId | string;
-    workoutId?: mongoose.Schema.Types.ObjectId | string;
-    name: string;
-    description?: string;
-    notes?: string;
-    restTime: number;
-    active: boolean;
-    createdAt?: Date;
-    updatedAt?: Date;
+  id?: string;
+  userId?: mongoose.Schema.Types.ObjectId | string;
+  name: string;
+  description?: string;
+  notes?: string;
+  restTime: number;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 export interface IExerciseModel {
-    user_id: mongoose.Schema.Types.ObjectId | string;
-    workout_id?: mongoose.Schema.Types.ObjectId | string;
-    name: string;
-    description?: string;
-    notes?: string;
-    rest_time: number;
-    active: boolean;
-    created_at?: Date;
-    updated_at?: Date;
+  user_id?: mongoose.Schema.Types.ObjectId | string;
+  name: string;
+  description?: string;
+  notes?: string;
+  rest_time: number;
+  created_at?: Date;
+  updated_at?: Date;
 }
 
-const ExerciseSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+const ExerciseSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
     name: { type: String, required: true },
     description: { type: String, required: false },
     notes: { type: String, required: false },
     rest_time: { type: Number, required: true, default: 120 }, // 2 minutes
-    active: { type: Boolean, required: true, default: true },
-}, {
+  },
+  {
     timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
-    }
-});
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
 
 export default model<IExerciseModel>("Exercise", ExerciseSchema);
-
--- backend/src/models/workout.model.ts
+++ backend/src/models/workout.model.ts
@@ -2,37 +2,56 @@ import mongoose, { model } from "mongoose";
 
 // e.g. Push Day, Pull Day, Leg Day, etc.
 export interface IWorkout {
-    id?: string;
-    userId: mongoose.Schema.Types.ObjectId | string;
-    planId: mongoose.Schema.Types.ObjectId | string;
-    title: string;
-    description?: string;
-    exercises: Array<mongoose.Schema.Types.ObjectId | string>; // References to Exercise model
-    createdAt?: Date;
-    updatedAt?: Date;
+  id?: string;
+  userId: mongoose.Schema.Types.ObjectId | string;
+  planId: mongoose.Schema.Types.ObjectId | string;
+  title: string;
+  description?: string;
+  exercises: Array<{
+    exerciseId: mongoose.Schema.Types.ObjectId | string;
+    isActive: boolean;
+  }>;
+  updatedAt?: Date;
 }
 
 // Interface for the workout model
 export interface IWorkoutModel {
-    _id?: string;
-    user_id: mongoose.Schema.Types.ObjectId | string;
-    plan_id: mongoose.Schema.Types.ObjectId | string;
-    title: string;
-    description?: string;
-    exercises: Array<mongoose.Schema.Types.ObjectId | string>;
+  _id?: string;
+  user_id: mongoose.Schema.Types.ObjectId | string;
+  plan_id: mongoose.Schema.Types.ObjectId | string;
+  title: string;
+  description?: string;
+  exercises: Array<{
+    exercise_id: mongoose.Schema.Types.ObjectId | string;
+    is_active: boolean;
+  }>;
 }
 
-const WorkoutSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    plan_id: { type: mongoose.Schema.Types.ObjectId, ref: "Plan", required: true },
+const WorkoutSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    plan_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Plan",
+      required: true,
+    },
     title: { type: String, required: true },
     description: { type: String, required: false },
-    exercises: { type: [mongoose.Schema.Types.ObjectId], ref: "Exercise", required: true },
-}, {
+    exercises: Array<{
+      exercise_id: { type: mongoose.Schema.Types.ObjectId; ref: "Exercise" };
+      is_active: { type: Boolean; default: true };
+    }>,
+  },
+  {
     timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
-    }
-});
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
 
 export default model<IWorkoutModel>("Workout", WorkoutSchema);
