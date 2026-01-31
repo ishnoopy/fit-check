79535dbd603141842d27dd0668ef52815732a381
Ruel Aldrin Guasa <114205020+ishnoopy@users.noreply.github.com>
2026-02-01 00:11:49 +0800

    chore: add insert many exercises function.

--- backend/src/repositories/exercise.repository.ts
+++ backend/src/repositories/exercise.repository.ts
@@ -1,6 +1,6 @@
-import type { FilterQuery } from 'mongoose';
-import ExerciseModel, { type IExercise } from '../models/exercise.model.js';
-import { toCamelCase, toSnakeCase } from '../utils/transformer.js';
+import type { FilterQuery } from "mongoose";
+import ExerciseModel, { type IExercise } from "../models/exercise.model.js";
+import { toCamelCase, toSnakeCase } from "../utils/transformer.js";
 
 export async function findAll(where: FilterQuery<IExercise> = {}) {
   const query = toSnakeCase(where);
@@ -11,12 +11,12 @@ export async function findAll(where: FilterQuery<IExercise> = {}) {
 export async function findOne(where: FilterQuery<IExercise> = {}) {
   const query = toSnakeCase(where);
   const exercise = await ExerciseModel.findOne(query).lean();
-  return exercise ? toCamelCase(exercise) as IExercise : null;
+  return exercise ? (toCamelCase(exercise) as IExercise) : null;
 }
 
 export async function findById(id: string) {
   const exercise = await ExerciseModel.findById(id).lean();
-  return exercise ? toCamelCase(exercise) as IExercise : null;
+  return exercise ? (toCamelCase(exercise) as IExercise) : null;
 }
 
 export async function createExercise(exercise: IExercise) {
@@ -25,14 +25,22 @@ export async function createExercise(exercise: IExercise) {
   return toCamelCase(doc.toObject()) as IExercise;
 }
 
+export async function createExercises(exercises: IExercise[]) {
+  const payloads = exercises.map((exercise) => toSnakeCase(exercise));
+  const docs = await ExerciseModel.insertMany(payloads);
+  return docs.map((doc) => toCamelCase(doc.toObject()) as IExercise);
+}
+
 export async function updateExercise(id: string, exercise: Partial<IExercise>) {
   const payload = toSnakeCase(exercise);
-  const doc = await ExerciseModel.findByIdAndUpdate(id, payload, { new: true, lean: true }).lean()
-  return doc ? toCamelCase(doc) as IExercise : null;
+  const doc = await ExerciseModel.findByIdAndUpdate(id, payload, {
+    new: true,
+    lean: true,
+  }).lean();
+  return doc ? (toCamelCase(doc) as IExercise) : null;
 }
 
 export async function deleteExercise(id: string) {
   const doc = await ExerciseModel.findByIdAndDelete(id).lean();
-  return doc ? toCamelCase(doc) as IExercise : null;
+  return doc ? (toCamelCase(doc) as IExercise) : null;
 }
-
