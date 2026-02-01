a160a253122c83eb3368ed42d6e450ab83f60591
Ruel Aldrin Guasa <114205020+ishnoopy@users.noreply.github.com>
2026-02-01 11:30:19 +0800

    chore: add exercise partial tests

--- backend/src/tests/exercise.test.ts
+++ backend/src/tests/exercise.test.ts
@@ -1,11 +1,45 @@
-import { describe, expect, it } from "vitest";
+import type { Mock } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import * as workoutRepo from "../repositories/workout.repository.js";
+
+vi.mock("jose", async () => {
+  const actual = await vi.importActual<any>("jose");
+  return {
+    ...actual,
+    jwtVerify: vi.fn().mockResolvedValue({
+      payload: { id: "6936810d0589536bab9e11b6", role: "user" },
+    }),
+  };
+});
+
+vi.mock("../repositories/workout.repository.js", () => ({
+  findAll: vi.fn(),
+}));
+
 import { app } from "../index.js";
 
-describe("GET /api/health", () => {
-  it("should return API is running message", async () => {
-    const response = await app.request("/api/health");
+describe("GET /api/workouts", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should return an empty array of workouts", async () => {
+    (workoutRepo.findAll as unknown as Mock).mockResolvedValue([]);
+    // Makes sure that findAll is being called in the service.
+    const response = await app.request("/api/workouts", {
+      headers: {
+        cookie: "access_token=valid_token",
+      },
+    });
+
     expect(response.status).toBe(200);
     const data = await response.json();
-    expect(data).toEqual({ message: "API is running" });
+    expect(data).toEqual({ success: true, data: [] });
+  });
+});
+
+describe("POST /api/workouts", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
   });
 });
